115d1c117e98eba2755926b1ddad493d73e80688
Miljan Samardzic <miljan.samardzic@bagpoint.com>
2021-02-14 15:54:30 +0100

    implement my products screen and product edit screen, WIP

--- screens/MyProductsScreen.js
+++ screens/MyProductsScreen.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { Avatar, Button, Card, Paragraph } from "react-native-paper";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 import { HeaderButton } from "../components";
+import { PRODUCTS } from "../data/products";
+import COLORS from "../consts/colors";
 
-const MyProductsScreen = () => {
+const MyProductsScreen = ({ navigation }) => {
   return (
     <View>
-      <Text>MyProductsScreen</Text>
+      <FlatList
+        data={PRODUCTS}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Product item={item} navigation={navigation} />
+        )}
+      />
     </View>
   );
 };
@@ -40,6 +50,60 @@ MyProductsScreen.navigationOptions = (navData) => {
   };
 };
 
-const styles = StyleSheet.create({});
+const Product = ({ item, navigation }) => {
+  return (
+    <Card style={styles.cardContainer} elevation={3}>
+      <Card.Title titleStyle={styles.cardTitle} title={item.title} />
+      <Button
+        style={styles.editButton}
+        onPress={() => {
+          navigation.navigate("EditProduct", {
+            mode: "edit",
+            id: item.id,
+          });
+        }}
+      >
+        <FontAwesome5 name="edit" size={22} color={COLORS.primary} />
+      </Button>
+      <View style={styles.contentContainer}>
+        <Card.Cover style={styles.cardCover} source={{ uri: item.images[0] }} />
+      </View>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    width: "85%",
+    borderRadius: 10,
+    margin: 20,
+    marginBottom: 0,
+    alignSelf: "center",
+  },
+  cardTitle: {
+    fontSize: 15,
+    color: COLORS.primary,
+  },
+  contentContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  cardCover: {
+    aspectRatio: 1.5,
+    height: 50,
+    marginBottom: 15,
+    marginLeft: 15,
+  },
+  cardActions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  editButton: {
+    position: "absolute",
+    right: 0,
+    top: 0,
+    marginRight: -10,
+  },
+});
 
 export default MyProductsScreen;
